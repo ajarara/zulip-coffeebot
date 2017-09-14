7aada35fe60b1da11107c032f5188a38f223389c
Ahmad Jarara <ajarara@jarmac.org>
2017-09-13 20:26:38 -0400

    prod: Add auth details to MANIFEST, v: 0.1.1 -> 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zulip-coffeebot',
-    version='0.1.1',
+    version='0.1.2',
     description='Coffee for all',
     url='https://github.com/alphor/zulip-coffee',
     author='Ahmad Jarara',
